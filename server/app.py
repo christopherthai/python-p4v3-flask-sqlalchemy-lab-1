d969fa746a67eb2019790bbb81691826dd922a6d
Christopher Thai <christopherthai.nyli@gmail.com>
2024-04-21 19:51:55 -0400

    Complete Lab

--- server/app.py
+++ server/app.py
@@ -7,21 +7,47 @@ from flask_migrate import Migrate
 from models import db, Earthquake
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    body = {'message': 'Flask SQLAlchemy Lab 1'}
+    body = {"message": "Flask SQLAlchemy Lab 1"}
     return make_response(body, 200)
 
+
 # Add views here
+@app.route("/earthquakes/<int:id>")
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {"message": f"Earthquake {id} not found."}
+        status = 404
+
+    return make_response(body, status)
 
 
-if __name__ == '__main__':
+# The route should have the form /earthquakes/magnitude/<float:magnitude>.
+# The view should query the database to get all earthquakes having a magnitude greater than or equal to the parameter value, and return a JSON response containing the count of matching rows along with a list containing the data for each row.
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def earthquake_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    count = len(earthquakes)
+    body = {
+        "count": count,
+        "quakes": [earthquake.to_dict() for earthquake in earthquakes],
+    }
+    return make_response(body, 200)
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
